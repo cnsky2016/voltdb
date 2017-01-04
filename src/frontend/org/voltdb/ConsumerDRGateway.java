d89d28f41801e5c0103f668d008c07416e5cd91e
Ning Shi <nshi@voltdb.com>
2017-01-04 16:03:54 -0500

    ENG:11693: DRROLE for multicluster XDCR
    - Return one row per conversation in DR producer and consumer node
      stats.
    
    - Roll DR producer and consumer node stats into DRROLE stats for each
      conversation.
    
    - This patch doesn't contain the work for the STOPPED state on the
      producer. It requires break replication to work on the producer.

--- src/frontend/org/voltdb/ConsumerDRGateway.java
+++ src/frontend/org/voltdb/ConsumerDRGateway.java
@@ -18,6 +18,7 @@
 package org.voltdb;
 
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.ExecutionException;
 
 // Interface through which the outside world can interact with the consumer side
@@ -27,7 +28,7 @@ public interface ConsumerDRGateway extends Promotable {
 
     void updateCatalog(CatalogContext catalog);
 
-    DRRoleStats.State getState();
+    Map<Byte, DRRoleStats.State> getStates();
 
     void initialize(boolean resumeReplication);
 
